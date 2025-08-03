021a8ce4af667f34c4a4086e5943258b4ab19f2f
musayub123 <DiryeAyub@gmail.com>
2025-08-03 16:16:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def upload_file():
     return jsonify({"document_type": "Insurance Certificate", "expiry_date": "2025-07-31"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
